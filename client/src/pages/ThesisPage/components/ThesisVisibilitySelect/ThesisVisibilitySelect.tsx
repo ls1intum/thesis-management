3308c7d63d65deb2908735f4ae122362fc9257e0
Marc Fett <marc.fett@web.de>
2025-05-07 02:20:07 +0200

    feat(thesis-visibility): update visibility descriptions to include research group context

--- client/src/pages/ThesisPage/components/ThesisVisibilitySelect/ThesisVisibilitySelect.tsx
+++ client/src/pages/ThesisPage/components/ThesisVisibilitySelect/ThesisVisibilitySelect.tsx
@@ -6,8 +6,9 @@ const ThesisVisibilitySelect = (props: SelectProps) => {
   const descriptions: Record<string, string> = {
     PUBLIC:
       'Every authenticated user can see the thesis. If thesis is completed, it will be publicly visible on the landing page',
-    STUDENT: 'All students that have a not completed thesis assigned can see the thesis',
-    INTERNAL: 'All advisors and supervisors of the chair can see the thesis',
+    STUDENT:
+      'All students that have a not completed thesis assigned  in your research group can see the thesis',
+    INTERNAL: 'All advisors and supervisors of the research group can see the thesis',
     PRIVATE: 'Only assigned users can see the thesis',
   }
 
