5a5a125a72f83bd4549f0949df9fdbbdb9b5d741
Ramona_Eckert <ge36moc@mytum.de>
2025-05-21 14:13:19 +0200

    Add an alert warning

--- client/src/pages/ResearchGroupSettingPage/components/DeleteMemberModal.tsx
+++ client/src/pages/ResearchGroupSettingPage/components/DeleteMemberModal.tsx
@@ -1,5 +1,6 @@
-import { Modal, Text, Button, Group } from '@mantine/core'
+import { Modal, Text, Button, Group, Alert, Stack } from '@mantine/core'
 import { ILightUser } from '../../../requests/responses/user'
+import { WarningCircle } from 'phosphor-react'
 
 type IDeleteMemberModalProps = {
   opened: boolean
@@ -11,16 +12,24 @@ type IDeleteMemberModalProps = {
 const DeleteMemberModal = ({ opened, onClose, onConfirm, member }: IDeleteMemberModalProps) => {
   return (
     <Modal opened={opened} onClose={onClose} title='Confirm Deletion' centered>
-      <Text mb='md'>
-        Are you sure you want to remove{' '}
-        {member ? `${member?.firstName} ${member?.lastName}` : 'this user'} from the research group?
-      </Text>
+      <Stack>
+        <Text>
+          Are you sure you want to remove{' '}
+          {member ? `${member?.firstName} ${member?.lastName}` : 'this user'} from the research
+          group?
+        </Text>
 
-      <Group justify='flex-end'>
-        <Button variant='outline' color='red' onClick={onConfirm}>
-          Remove
-        </Button>
-      </Group>
+        <Alert variant='light' color='orange' title='Important' icon={<WarningCircle size={16} />}>
+          The removed member might have open theses and topics. Please ensure that you have
+          considered this before proceeding.
+        </Alert>
+
+        <Group justify='flex-end'>
+          <Button variant='outline' color='red' onClick={onConfirm}>
+            Remove
+          </Button>
+        </Group>
+      </Stack>
     </Modal>
   )
 }
