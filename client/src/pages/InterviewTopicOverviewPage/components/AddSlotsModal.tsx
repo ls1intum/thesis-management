8b08cccf4822aad8335d62e136ae577d72bc4511
Ramona_Eckert <ge36moc@mytum.de>
2025-10-30 13:42:26 +0100

    Add Calendar and Slot items

--- client/src/pages/InterviewTopicOverviewPage/components/AddSlotsModal.tsx
+++ client/src/pages/InterviewTopicOverviewPage/components/AddSlotsModal.tsx
@@ -7,10 +7,15 @@ import {
   Divider,
   Flex,
   ScrollArea,
+  Button,
+  Accordion,
+  Text,
+  Center,
 } from '@mantine/core'
 import { Calendar } from '@mantine/dates'
 import { useState } from 'react'
 import dayjs from 'dayjs'
+import { CalendarBlankIcon } from '@phosphor-icons/react'
 
 interface IAddSlotsModalProps {
   slotModalOpen: boolean
@@ -34,11 +39,14 @@ const AddSlotsModal = ({ slotModalOpen, setSlotModalOpen }: IAddSlotsModalProps)
       centered
       size='1000px'
       opened={slotModalOpen}
-      onClose={() => setSlotModalOpen(false)}
+      onClose={() => {
+        setSlotModalOpen(false)
+        setSelected([])
+      }}
       title={<Title order={3}>Add Interview Slot</Title>}
     >
-      <Stack gap={'2rem'}>
-        <Group justify='flex-start' align='center'>
+      <Stack gap={'2.5rem'} h={'100%'}>
+        <Group justify='flex-start' align='center' gap={'2rem'}>
           <Stack gap={'0.25rem'}>
             <Title order={6} c={'dimmed'}>
               Interview Length
@@ -69,24 +77,67 @@ const AddSlotsModal = ({ slotModalOpen, setSlotModalOpen }: IAddSlotsModalProps)
           gap={{ base: '1rem', md: '2rem' }}
           style={{ overflow: 'auto' }}
         >
-          <Stack w={{ base: '100%', sm: 'fit-content' }} h={'100%'} gap={'1.5rem'}>
+          <Stack w={{ base: '100%', sm: 'fit-content' }} h={'100%'} align='center'>
+            <Title order={5}>Select Dates</Title>
             <Calendar
-              getDayProps={(date) => ({
-                selected: selected.some((s) => dayjs(s).isSame(date, 'day')),
-                onClick: () => handleSelect(new Date(date)),
-              })}
+              minDate={new Date()}
+              getDayProps={(date) => {
+                const isPast = dayjs(date).isBefore(dayjs(), 'day')
+                return {
+                  disabled: isPast,
+                  selected: selected.some((s) => dayjs(s).isSame(date, 'day')),
+                  onClick: isPast ? undefined : () => handleSelect(new Date(date)),
+                }
+              }}
               highlightToday
               size={'md'}
             />
           </Stack>
           <Divider orientation='vertical' />
-          <Stack h={'100%'} flex={1} gap={'1.5rem'}>
+          <Stack h={'100%'} flex={1}>
             <Title order={5}>Create Slots</Title>
-            <ScrollArea h={'100%'} w={'100%'} type={'hover'} offsetScrollbars>
-              <Stack></Stack>
-            </ScrollArea>
+            {selected.length === 0 ? (
+              <Center h={'100%'} mih={'250px'}>
+                {/* TODO: FIGURE OUT HOW TO USE 100% of the height */}
+                <Stack justify='center' align='center' h={'100%'}>
+                  <CalendarBlankIcon size={40} />
+                  <Stack gap={'0.5rem'} justify='center' align='center'>
+                    <Title order={6}>No Date selected</Title>
+                    <Text c='dimmed'>Please select one or multiple dates in the calendar</Text>
+                  </Stack>
+                </Stack>
+              </Center>
+            ) : (
+              <ScrollArea h={'100%'} w={'100%'} type={'hover'} offsetScrollbars>
+                <Stack mih={'350px'} justify='space-between'>
+                  {/* TODO: FIGURE OUT HOW TO USE 100% of the height */}
+                  <Accordion chevronPosition='left' variant={'separated'}>
+                    {selected
+                      .sort((a, b) => a.getTime() - b.getTime())
+                      .map((date) => (
+                        <Accordion.Item key={date.toDateString()} value={date.toDateString()}>
+                          <Accordion.Control>
+                            <Text>{dayjs(date).format('DD.MM.YYYY')}</Text>
+                          </Accordion.Control>
+                          <Accordion.Panel>
+                            <Stack>
+                              <Text>Interview Length:</Text>
+                              <Text>Interview Break:</Text>
+                            </Stack>
+                          </Accordion.Panel>
+                        </Accordion.Item>
+                      ))}
+                  </Accordion>
+                  <Button variant='outline' size='xs'>
+                    Same Slots for all days
+                  </Button>
+                </Stack>
+              </ScrollArea>
+            )}
           </Stack>
         </Flex>
+
+        <Button fullWidth>Save Slots</Button>
       </Stack>
     </Modal>
   )
