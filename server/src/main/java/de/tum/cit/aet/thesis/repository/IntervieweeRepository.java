e5290c350fb01e154671c37d56d7c732d87b41bf
Ramona_Eckert <ge36moc@mytum.de>
2026-01-29 11:18:00 +0100

    When someone is completed he only shows up in that category

--- server/src/main/java/de/tum/cit/aet/thesis/repository/IntervieweeRepository.java
+++ server/src/main/java/de/tum/cit/aet/thesis/repository/IntervieweeRepository.java
@@ -19,8 +19,8 @@ public interface IntervieweeRepository extends JpaRepository<Interviewee, UUID> 
           AND (
             :state IS NULL
             OR (
-                (:state = 'UNCONTACTED' AND i.lastInvited IS NULL)
-             OR (:state = 'SCHEDULED'   AND SIZE(i.slots) > 0)
+                (:state = 'UNCONTACTED' AND i.lastInvited IS NULL and i.score IS NULL)
+             OR (:state = 'SCHEDULED'   AND SIZE(i.slots) > 0 AND i.score IS NULL)
              OR (:state = 'COMPLETED'   AND i.score IS NOT NULL)
              OR (:state = 'INVITED'     AND i.lastInvited IS NOT NULL AND SIZE(i.slots) = 0 AND i.score IS NULL)
             )
