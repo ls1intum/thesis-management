004b328730af431bb4e7d61f7294bd222f2c2b80
Stephan Krusche <krusche@tum.de>
2026-03-03 18:27:05 +0100

    Remove matriculation number sync

--- server/src/main/java/de/tum/cit/aet/thesis/repository/UserRepository.java
+++ server/src/main/java/de/tum/cit/aet/thesis/repository/UserRepository.java
@@ -51,9 +51,6 @@ public interface UserRepository extends JpaRepository<User, UUID> {
 	List<User> getRoleMembers(@Param("roles") Set<String> roles,
 							@Param("researchGroupId") UUID researchGroupId);
 
-	@Query("SELECT u.id FROM User u WHERE u.matriculationNumber IS NULL")
-	List<UUID> findIdsWithoutMatriculationNumber();
-
 	@Query("SELECT u FROM User u WHERE u.avatar IS NULL OR u.avatar = ''")
 	List<User> findAllByAvatarIsNullOrAvatarIsEmpty();
 
